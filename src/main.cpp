e96f511cf2c92ddb851ed3cf40bf47848f72535d
Antony Leveugle <antoine.leveugle@yahoo.fr>
2015-11-18 09:59:28 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2015, STEREOLABS.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
+
 /**********************************
 ** Using the ZED with Oculus Rift
 **********************************/
@@ -223,7 +243,7 @@ int main(int argc, char **argv)
 	
 	// Initialize hit value
 	float hit = 0.02f;
-	// Initialize a boolean that will be used to stop the application’s loop and another one to pause/unpause rendering
+	// Initialize a boolean that will be used to stop the applicationÂ’s loop and another one to pause/unpause rendering
 	bool end = false;
 	bool refresh = true;
 	// SDL variable that will be used to store input events
